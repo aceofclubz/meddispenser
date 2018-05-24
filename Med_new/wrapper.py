a627e60504be7c01731915ed3f5575d12b7122e7
aceofclubz <gmsperias@gmail.com>
2018-05-24 21:15:30 +0800

    Added wrapper for SQL integration

--- Med_new/wrapper.py
+++ Med_new/wrapper.py
@@ -1,23 +1,37 @@
 from datetime import date, datetime, timedelta
+import re
 import mysql.connector as connection
 
-class wrapper:
+class Wrapper:
 
     def __init__(self):
         self.cnx = connection.MySQLConnection(user='root', password='root',
                                               host='localhost',
-                                              database='medvendo')
-        self.cursor = self.cnx.cursor(dictionary=True, buffered=True)
+                                              database='Dispenser')
+        self.cnx.autocommit = True
+        self.cursor = self.cnx.cursor(dictionary=True,buffered=True,)
 
     def insert(self, tablename, **kwargs):
         x = *kwargs,
         y = *kwargs.values(),
-        query = "INSERT INTO " + tablename + " " + str(x) + " VALUES " + str(y)
+        query = "INSERT INTO " + tablename + " " + re.sub("['\"]","",str(x)) + " VALUES " + str(y)
+        print(query)
         self.cursor.execute(query)
         return
 
     def select(self, tablename, **kwargs):
         x = *kwargs,
         y = *kwargs.values(),
-        query = "SELECT * FROM " + tablename + " WHERE " + str(x[0]) + " = " + str(y[0])
-        return self.cursor.execute(query)
+        query = "SELECT * FROM " + tablename + " WHERE " + str(x[0]) + " = \'" + y[0] + "\'"
+        print(query)
+        self.cursor.execute(query)
+        return self.cursor
+
+    def update(self, tablename, field, value, **kwargs):
+        x = *kwargs,
+        y = *kwargs.value()
+        query = "UPDATE " + tablename + "SET " + str(x[0]) + " = \'" + y[0] + "\' WHERE " + field + " = \'" + value + "\'"
+        
+
+    def testing(self):
+        print('bogo')
