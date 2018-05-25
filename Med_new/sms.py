c7e0a3f36abb3226b2769d22508eb9fce23d99d0
aceofclubz <34234555+aceofclubz@users.noreply.github.com>
2018-05-25 16:38:50 +0800

    Added sms
    sms.py will just be imported and serve as a function

---
+++ Med_new/sms.py
@@ -0,0 +1,16 @@
+from twilio.rest import TwilioRestClient
+
+def send_msg(text):
+
+    act_sid = "AC036f1a9ceb42f28e68e1ceb9a45e31de"
+    autho_token = "e5337a7bc963f6e7af386433a6314a9c"
+
+    twilio_no = "+19202801868"
+
+    client = TwilioRestClient(act_sid, autho_token)
+
+    message = client.messages.create(body=text,
+                                     from_=twilio_no,
+                                     to="+639286382077",
+                                     )
+
