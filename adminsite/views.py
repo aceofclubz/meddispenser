e01cb981a3aa9dfc9b4185b807c1610a361695ff
gh0stsh0t <rjperias0213@gmail.com>
2019-01-27 16:00:35 +0800

    added templates and some preliminary code

--- adminsite/views.py
+++ adminsite/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import *
 
 # Create your views here.
+
+def requestAjax(reqeust):
+    data = {‘is_valid’: False,}
+    if request.is_ajax():
+        message = request.POST.get(‘message’)
+    if message == ‘I want an AJAX response’:
+        data.update(is_valid=True)
+        data.update(‘response’=’This is the response you wanted’)
+    return JsonResponse(data)
