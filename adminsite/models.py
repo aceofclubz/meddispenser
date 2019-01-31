91e12f164f711982edec1fc60ff808af095c8318
RJ Perias <rjperias0213@gmail.com>
2019-01-31 22:30:44 +0800

    created reports webapp using the django framework (#1)
    * started django app
    
    * added templates and some preliminary code
    
    * created login
    
    * started on something
    
    * started on templating
    
    * testing some things
    
    * i have a good template now
    
    * finished templates, ToDO: make nice design
    
    * fixed errors

---
+++ adminsite/models.py
@@ -0,0 +1,49 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+
+
+class Admin(models.Model):
+    adminid = models.AutoField(db_column='adminID', primary_key=True)  # Field name made lowercase.
+    adminuser = models.CharField(db_column='adminUser', max_length=45)  # Field name made lowercase.
+    adminpass = models.CharField(db_column='adminPass', max_length=45)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'admin'
+
+
+class Medicine(models.Model):
+    mid = models.AutoField(primary_key=True)
+    medname = models.CharField(db_column='medName', max_length=45, blank=True, null=True)  # Field name made lowercase.
+    count = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'medicine'
+
+
+class Transaction(models.Model):
+    tid = models.AutoField(primary_key=True)
+    medid = models.ForeignKey(Medicine, models.DO_NOTHING, db_column='medID', blank=True, null=True)  # Field name made lowercase.
+    userid = models.ForeignKey('User', models.DO_NOTHING, db_column='userID', blank=True, null=True)  # Field name made lowercase.
+    datetime = models.DateTimeField(blank=True, null=True)
+    presentcount = models.IntegerField(db_column='presentCount', blank=True, null=True)  # Field name made lowercase.
+    class Meta:
+        managed = False
+        db_table = 'transaction'
+
+
+class User(models.Model):
+    uid = models.CharField(primary_key=True, max_length=43)
+    fn = models.CharField(max_length=45, blank=True, null=True)
+    ln = models.CharField(max_length=45, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'user'
---
+++ adminsite/views.py
@@ -0,0 +1,76 @@
+from django.shortcuts import render, redirect
+from django.http import JsonResponse, HttpResponse
+from .models import *
+from .forms import *
+
+
+def login(request):
+    context = {}
+    if request.method == 'POST':
+        m = Admin.objects.get(adminuser=request.POST['adminuser'])
+        if m.adminpass == request.POST['adminpass']:
+            request.session['admin_id'] = m.adminid
+            request.session['user'] = m.adminuser
+            return redirect('/')
+        else:
+            context['fail'] = True
+            return render(request, 'signin.html', context)
+    else:
+        context['field'] = AdminForm()
+        return render(request, 'signin.html', context)
+
+
+def logout(request):
+    try:
+        del request.session['admin_id']
+        del request.session['user']
+    except KeyError:
+        pass
+    return redirect('/')
+
+
+def index(request):
+    try:
+        context = {}
+        context['user'] = request.session['user']
+        context['transactions'] = Transaction.objects.all()
+        return render(request, 'index.html', context)
+    except KeyError:
+        return redirect('/login')
+
+
+def users(request):
+    context = {}
+    context['users'] = User.objects.all()
+    return render(request, 'user.html', context)
+
+def transact(request):
+    context = {}
+    context['transactions'] = Transaction.objects.all()
+    return render(request, 'transaction.html', context)
+
+def transact_d(request):
+    context = {}
+    start_date = request.POST['start']
+    end_date = request.POST['end']
+    context['transactions'] = Transaction.objects.filter(datetime=(start_date, end_date))
+    return render(request, 'transaction.html', context)
+
+
+def transact_u(request, user_id):
+    context = {}
+    user = User.objects.get(uid=user_id)
+    context['transactions'] = Transaction.objects.filter(userid=user)
+    return render(request, 'transaction.html', context)
+
+
+def transact_med(request, med_id):
+    context = {}
+    context['transactions'] = Transaction.objects.filter(medid=med_id)
+    return render(request, 'transaction.html', context)
+
+def medicine(request):
+    context = {}
+    context['medicines'] = Medicine.objects.all()
+    return render(request, 'medicine.html', context)
+
