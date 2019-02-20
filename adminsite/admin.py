71e8e0aee65ca9a6c84ce32ce78a28e70726550b
gh0stsh0t <rjperias0213@gmail.com>
2019-02-21 02:41:04 +0800

    removed add user functions

--- adminsite/admin.py
+++ adminsite/admin.py
@@ -4,5 +4,4 @@ from .models import Admin, Medicine, Transaction, User
 # Register your models here.
 admin.site.register(Admin)
 admin.site.register(Medicine)
-admin.site.register(Transaction)
 admin.site.register(User)
